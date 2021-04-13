4cb8708151863fbafeb7997944036d70b7a5219d
robbyardha <robbyardha@gmail.com>
2021-04-14 04:18:31 +0700

    add maintenance mode

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /*
 | -------------------------------------------------------------------------
@@ -11,3 +11,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |	https://codeigniter.com/user_guide/general/hooks.html
 |
 */
+$hook['pre_system'][] = array(
+    'class' => 'maintenance_hook',
+    'function' => 'offline_check',
+    'filename' => 'maintenance_hook.php',
+    'filepath' => 'hooks'
+);
---
+++ application/views/maintenance-mode/maintenance.php
@@ -0,0 +1,89 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
+<!doctype html>
+<html lang="">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>SORRY WE STILL MAINTENANCE</title>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <!-- Styles -->
+    <style>
+        html,
+        body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Raleway', sans-serif;
+            font-weight: 100;
+            height: 100vh;
+            margin: 0;
+        }
+
+        #img-load {
+            width: 135px;
+            height: 150px;
+            background: url(../assets/images/maintain.svg);
+            /* background: url(../assets/images/profile/admin.png); */
+        }
+
+        .full-height {
+            height: 100vh;
+        }
+
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+
+        .position-ref {
+            position: relative;
+        }
+
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
+
+        .content {
+            text-align: center;
+        }
+
+        .title {
+            font-size: 84px;
+        }
+
+        .links>a {
+            color: #636b6f;
+            padding: 0 25px;
+            font-size: 12px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
+
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="flex-center position-ref full-height">
+        <div class="content">
+            <img id="img-load" src="./assets/images/maintain.svg" alt="" srcset="">
+            <!-- <img id="img-load" src="../assets/images/profile/admin.png" alt="" srcset=""> -->
+            <div class="title m-b-md">
+                Sorry, We Still Maintenance!
+            </div>
+            <h3>Please Wait Until Finish!</h3>
+            <h5>&copy;ARDHACODES</h5>
+        </div>
+    </div>
+</body>
+
+</html>
