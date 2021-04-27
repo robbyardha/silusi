5643841fbf72fcb737707703f78332ec820263ea
papamuda99 <nouvalrahman28@gmail.com>
2021-04-27 12:25:24 +0700

    cetak skl update

--- application/views/user/content/cetak_skl.php
+++ application/views/user/content/cetak_skl.php
@@ -1,20 +1,274 @@
+<style type="text/css">
+    @media print {
+        input.noPrint {
+            display: none;
+        }
+    }
+    
+    .table1 tr{
+        font-family: Arial;
+        font-size: 12pt;
+        width: 200px;
+        
+    }
+    
+    .table2{
+        width: 550px;
+        text-align: center;
+        border-spacing: 0px;;
+    }
+    
+    .box1{
+        height: 4cm;
+        width: 3cm;
+        right: 1600px;
+        position: absolute;
+        
+        margin-top: 50px;
+        border-style: solid;
+        
+    }
+    
+    .box1 .a {
+        margin-top: 2cm;
+    }
+    .raport th {
+        border: 1px solid #000;
+    }
+
+    .raport td {
+        border: 1px solid #000;
+        font-weight: normal;
+    }
+
+    .khusus td {
+        text-align: center;
+        border: 2px solid #000;
+    }
+
+    .khusus th {
+        border: 2px solid #000;
+    }
+
+    td {
+        height: 30px;
+    }
+
+    .pagebreak {
+        page-break-after: always;
+    }
+
+    .pagebreak {
+        page-break-after: always;
+    }
+
+    .header td {
+        padding: 0px;
+        height: 25px;
+    }
+</style>
+
+
+
+
+
+
+
 <form><input class="noPrint" type="button" value="Print" onclick="window.print()"></form>
-<div style='width:650px;padding:7px;'>
-    <div style='width:550px;font-family:arial;font-weight:bold;font-size:15pt;text-align:center;'>
+<div class="cetak-skl" style='width: 750px;padding:7px;'>
+    <div style='width: 750px;font-family:arial;font-weight:bold;font-size:15pt;text-align:center;'>
         SURAT KETERANGAN LULUS
+        <hr>
+        Nomor : 375/KET/IV.4/SMAM.1/f/2021
     </div>
-    <div style='width:550px;font-family:arial;font-weight:bold;font-size:15pt;text-align:center;'>
-        <hr style='width=550px;height=5px;'>
-    </div>
-    <div style='margin-top:-7px;text-align:right;'>
-    <div style='width:550px;font-family:arial;font-size:11pt;text-align:left;'>
+    
+    
+    <!--<div style='margin-top:27px;text-align:right;'>-->
+    <div style='width: 750px;font-family:arial;font-size:11pt;text-align:left;'>
         Kepala SMA Muhammadiyah 1 Taman - Sidoarjo selaku  ketua penyelenggara Ujian Sekolah Tahun Pelajaran 2020/2021 berdasarkan : 
     </div>
-    <div style='width:500px;font-family:arial;font-size:11pt;text-align:left;'>
-         1. Ketuntasan dari seluruh program pembelajaran kurikulum 2013
-         2. kriteria kelulusan dari satuan pendidikan sesuai dengan peraturan perunfang undangan 
+    <div style='width: 750px;font-family:arial;font-size:11pt;text-align:left;margin-left: 19px;;'>
+         1. Ketuntasan dari seluruh program pembelajaran kurikulum 2013 <br>
+         2. kriteria kelulusan dari satuan pendidikan sesuai dengan peraturan perundang undangan <br>
+         3. Rapat Pleno Dewan Penduduk Tentang Kelulusan dan pengumuman kelulusan pada tanggal 3 Mei 2021<br>
+         <br>
     </div>
-    <hr style='width:550px;height:3px;'>
+    <div style='width: 750px;font-family:arial;font-size:11pt;text-align:left;'>
+        Menerangkan Bahwa : 
+    </div>
+    <div style='width: 750px;font-family:arial;font-size:11pt;text-align:left;margin-left:20px;'>
+        <table class='table1' cellspacing='0' >
+            <tr>
+                <td>Nama </td>
+                <td>: </td>
+                <td>Nouval  </td>
+            </tr>
+            <tr>
+                <td>Tempat/Tanggal Lahir </td>
+                <td>: </td>
+                <td>Jombang </td>
+            </tr>
+            <tr>
+                <td>Kelas/No. Induk </td>
+                <td>: </td>
+                <td>$Nama </td>
+            </tr>
+            <tr>
+                <td>Nama </td>
+                <td>: </td>
+                <td>$Nama </td>
+            </tr>
+            <tr>
+                <td>Nama </td>
+                <td>: </td>
+                <td>$Nama </td>
+            </tr>
+            <tr>
+                <td>Nama </td>
+                <td>: </td>
+                <td>$Nama </td>
+            </tr>
+            <tr>
+                <td>Nama </td>
+                <td>: </td>
+                <td>$Nama </td>
+            </tr>
+            <tr>
+                <td>Nama </td>
+                <td>: </td>
+                <td>$Nama </td>
+            </tr>
+
+        </table>
+
+    </div>
+
+    <div><br>
+        Dengan Nilai sebagai berikut :
+    </div>
+    <table class='table2' border='1'>
+        <tr>
+            <td>No</td>
+            <td>Mata Pelajaran</td>
+            <td>NR</td>
+            <td>NSP</td>
+            <td>NUSP</td>
+
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Bahasa Indonesia</td>
+            <td>85</td>
+            <td>85</td>
+            <td>85</td>
+
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Bahasa Indonesia</td>
+            <td>85</td>
+            <td>85</td>
+            <td>85</td>
+
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Bahasa Indonesia</td>
+            <td>85</td>
+            <td>85</td>
+            <td>85</td>
+
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Bahasa Indonesia</td>
+            <td>85</td>
+            <td>85</td>
+            <td>85</td>
+
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Bahasa Indonesia</td>
+            <td>85</td>
+            <td>85</td>
+            <td>85</td>
+
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Bahasa Indonesia</td>
+            <td>85</td>
+            <td>85</td>
+            <td>85</td>
+
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Bahasa Indonesia</td>
+            <td>85</td>
+            <td>85</td>
+            <td>85</td>
+
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Bahasa Indonesia</td>
+            <td>85</td>
+            <td>85</td>
+            <td>85</td>
+
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Bahasa Indonesia</td>
+            <td>85</td>
+            <td>85</td>
+            <td>85</td>
+
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Bahasa Indonesia</td>
+            <td>85</td>
+            <td>85</td>
+            <td>85</td>
+
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Bahasa Indonesia</td>
+            <td>85</td>
+            <td>85</td>
+            <td>85</td>
+
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Bahasa Indonesia</td>
+            <td>85</td>
+            <td>85</td>
+            <td>85</td>
+
+        </tr>
+        <tr>
+            
+            <td></td>
+            <td>Rata - Rata</td>
+            <td>85</td>
+            <td>85</td>
+            <td>85</td>
+            
+        </tr>
+
+    </table> 
+    <div class="box1">
+
+        <a><center><br><br> Pas Foto 3 x 4 Cm</center></a>
+
+    </div>
+
+
 </div>
     
         
