ac3aeccbbed98c0cba5d58ce01803fb3d5079e33
Robby Firmansyah Ardha <robbyardha@gmail.com>
2022-04-29 12:49:15 +0700

    backup skl logic rata rata

--- application/views/user/content/cetak_skl.php
+++ application/views/user/content/cetak_skl.php
@@ -461,29 +461,7 @@
                         <td class="kel-center"><?= $lintasminat['nusp'] ?></td>
                     </tr>
                 <?php endforeach ?>
-                <tr>
-                    <td colspan="2"><strong>Rata Rata</strong></td>
-                    <td colspan="1" class="kel-center"><Strong>
-                            <?php
-                            // var_dump($nilai_sum);
-                            // die;
-                            if ($nilai_mapel[0]['kelas'] == "IPA") {
-                                $sum_avg_ipa = $nilai_sum_nusp[0]['SUM(nusp)'];
-                                // $sum_avg_ipa = $nilai_mapel[0]['nsp'] + $nilai_mapel[1]['nsp'] + $nilai_mapel[2]['nsp'] + $nilai_mapel[3]['nsp'] + $nilai_mapel[4]['nsp'] + $nilai_mapel[5]['nsp'] + $nilai_mapel[6]['nsp'] + $nilai_mapel[7]['nsp'] + $nilai_mapel[8]['nsp'] + $nilai_mapel[9]['nsp'] + $nilai_mapel[10]['nsp'] + $nilai_mapel[11]['nsp'] + $nilai_mapel[12]['nsp'] + $nilai_mapel[13]['nsp'] +  $nilai_mapel[18]['nsp'];
-                                $count_mapel_ipa = $getnumrowsmapel;
-                                $result = $sum_avg_ipa / $count_mapel_ipa;
-                            } elseif ($nilai_mapel[0]['kelas'] == "IPS") {
-                                // $sum_avg_ips = $nilai_mapel[0]['nsp'] + $nilai_mapel[1]['nsp'] + $nilai_mapel[2]['nsp'] + $nilai_mapel[3]['nsp'] + $nilai_mapel[4]['nsp'] + $nilai_mapel[5]['nsp'] + $nilai_mapel[6]['nsp'] + $nilai_mapel[7]['nsp'] + $nilai_mapel[8]['nsp'] + $nilai_mapel[9]['nsp'] + $nilai_mapel[14]['nsp'] + $nilai_mapel[15]['nsp'] + $nilai_mapel[16]['nsp'] + $nilai_mapel[17]['nsp'] + $nilai_mapel[18]['nsp'];
-                                $sum_avg_ips =  $nilai_sum_nusp[0]['SUM(nusp)'];
-                                $count_mapel_ips = $getnumrowsmapel;
-                                $result = $sum_avg_ips / $count_mapel_ips;
-                            }
-                            // echo (ceil($result, 3));
-                            // echo (floor($result, 3));
-                            // echo (round($result, 3));
-                            echo (number_format($result, 2));
-                            ?></strong></td>
-                </tr>
+
             </tbody>
         </table>
         <br>
